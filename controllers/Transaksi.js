0a484f7abdafddcbe3a0f379328a79295f8c14f3
denidarmayana <denidarmayana88@gmail.com>
2024-07-14 13:24:41 +0800

    callback

--- controllers/Transaksi.js
+++ controllers/Transaksi.js
@@ -5,6 +5,8 @@ const Pulsa = require('../digiflazz/Pulsa');
 const Bayar = require('../digiflazz/BayarTagihan');
 const { messaging } = require('firebase-admin');
 const { query } = require('../utils/database');
+const fcm = require("../utils/firebase");
+
 exports.TransferSaldo = async(req,res)=>{
 	const {receiver_name, sender_name,amount, reff_receiver,phone_receiver,reff, phone, invoice} = req.body
 	const pengiriman = InsertDB.PengirimanSaldo(receiver_name, amount, reff, phone, invoice)
@@ -47,6 +49,7 @@ exports.beliPulsa = async(req,res)=>{
 	try {
         const hasil = await Pulsa.Beli(produk, number, invoice);
 		const penerimaan = InsertDB.Prabayar(product_name,produk, amount, reff, number, invoice)
+		console.log(hasil)
         res.json(hasil);
     } catch (error) {
         res.status(500).json({ error: error });
@@ -101,8 +104,66 @@ exports.bayarTagihan = async(req,res)=>{
 	try {
         const hasil = await Bayar.Bayar(produk, number, invoice);
 		const penerimaan = InsertDB.Pascabayar(product_name,produk, amount, reff, number, invoice)
+		await bonus.BonusPrabayar(reff,invoice)
         res.json(hasil);
     } catch (error) {
         res.status(500).json({ error: error });
     }
 }
+const BonusTopUp = [
+    { level: 1, amount: 700 },
+    { level: 2, amount: 200 },
+    { level: 3, amount: 100 },
+    { level: 4, amount: 100 },
+    { level: 5, amount: 100 }
+];
+exports.callbackFlip = async(req,res)=>{
+	const {invoice} = req.body
+	try {
+        const trx = await query("SELECT * FROM transaksi WHERE invoice = ?",[invoice]);
+		const users = await query("SELECT * FROM members WHERE reff = ?",[trx[0].members])
+		await query("UPDATE transaksi SET status = 1 WHERE invoice = ?",[invoice])
+		const newBalance = parseFloat(users[0].sososo) + parseFloat(trx[0].sale)
+		await query("UPDATE members SET sososo = ?, trx_7 = ? WHERE reff= ?",[newBalance, trx[0].sale, trx[0].members])
+		fcm.sendFCM(users[0].token, "2", invoice, "Pengisian Saldo", `Pengisian Saldo #${invoice}\nsebesar ${trx[0].sale} berhasil ditambahkan ke saldo anda`);
+		for (let i = 1; i <= BonusTopUp.length; i++) {
+            const uplineField = (i === 1) ? 'upline' : `upline_${i}`;
+            const bonus = BonusTopUp.find(b => b.level === i);
+            if (users[0][uplineField] > 0 && bonus) {
+                await IsiBonus(title, bonus.amount, users[0][uplineField], invoice);
+                await sendNotification(users[0][uplineField], invoice, title, users[0].name,bonus.amount);
+            }
+        }
+        res.json(hasil);
+    } catch (error) {
+        res.status(500).json({ error: error });
+    }
+}
+
+const sendNotification = async (upline, invoice, title, senderName,amount) => {
+    try {
+        const down = await query("SELECT * FROM members WHERE reff = ?", [upline]);
+        if (down.length > 0) {
+            fcm.sendFCM(down[0].token, "2", invoice, title, `Voucher #${invoice}\nAnda mendapatkan voucher sebesar ${amount} dari transaksi ${senderName}`);
+        }
+    } catch (fcmError) {
+        console.error("Error sending FCM notification:", fcmError, { upline, invoice });
+    }
+};
+
+const IsiBonus = async (title, amount, receiver, invoice) => {
+    const deskripsi = `${title} sebesar ${formatRupiah(amount)}`;
+    const sql = `
+        INSERT INTO transaksi
+        (date, invoice, members, product, customers, sale, price, \`desc\`, type, status)
+        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    `;
+    const values = [dateNow, invoice, receiver, title, receiver, amount, amount, deskripsi, 6, 1];
+    try {
+        const insert = await query(sql, values);
+        return insert;
+    } catch (error) {
+        console.error("Error executing query:", error, { values });
+        throw error;
+    }
+};
--- utils/bonus.js
+++ utils/bonus.js
@@ -1,5 +1,6 @@
 const { query } = require("../utils/database");
 const moment = require('moment-timezone');
+const fcm = require("./firebase");
 
 const jakartaTime = moment.tz("Asia/Jakarta");
 const dateTimeNow = jakartaTime.format("YYYY-MM-DD HH:mm:ss");
@@ -13,6 +14,18 @@ const formatRupiah = (number) => {
         maximumFractionDigits: 0
     }).format(number).replace(/\u00A0/, ' ').replace('Rp', 'Rp ');
 };
+
+const BonusPraPasca = [
+    { level: 1, amount: 100 },
+    { level: 2, amount: 100 },
+    { level: 3, amount: 50 },
+    { level: 4, amount: 100 },
+    { level: 5, amount: 50 },
+    { level: 6, amount: 50 },
+    { level: 7, amount: 50 },
+    { level: 8, amount: 50 },
+    { level: 9, amount: 100 },
+];
 
 const IsiBonus = async (title, amount, receiver, invoice) => {
     const deskripsi = `${title} sebesar ${formatRupiah(amount)}`;
@@ -30,33 +43,43 @@ const IsiBonus = async (title, amount, receiver, invoice) => {
         throw error;
     }
 };
-const fcm = require("./firebase")
 
-exports.BonusPrabayar = async (reff, invoice) => {
+const sendNotification = async (upline, invoice, title, senderName,amount) => {
+    try {
+        const down = await query("SELECT * FROM members WHERE reff = ?", [upline]);
+        if (down.length > 0) {
+            fcm.sendFCM(down[0].token, "2", invoice, title, `Voucher #${invoice}\nAnda mendapatkan voucher sebesar ${amount} dari transaksi ${senderName}`);
+        }
+    } catch (fcmError) {
+        console.error("Error sending FCM notification:", fcmError, { upline, invoice });
+    }
+};
+
+const distributeBonus = async (reff, invoice, title) => {
     try {
         const users = await query("SELECT * FROM members WHERE reff = ?", [reff]);
         if (users.length === 0) {
             console.error("No users found with the provided reference:", reff);
             return;
         }
-
-        for (let i = 1; i < 6; i++) {
+        for (let i = 1; i <= BonusPraPasca.length; i++) {
             const uplineField = (i === 1) ? 'upline' : `upline_${i}`;
-            
-            if (users[0][uplineField] > 0) {
-                await IsiBonus("Voucher Prabayar", 50, users[0][uplineField], invoice);
-                try {
-                    const down = await query("SELECT * FROM members WHERE reff = ?", [users[0][uplineField]]);
-                    if (down.length > 0) {
-                        fcm.sendFCM(down[0].token, "2", invoice, "Voucher Prabayar", `Voucher #${invoice}\nAnda mendapatkan voucher sebesar 50 dari transaksi ${users[0].name}`);
-                    }
-                } catch (fcmError) {
-                    console.error("Error sending FCM notification:", fcmError, { upline: users[0][uplineField], invoice });
-                }
+            const bonus = BonusPraPasca.find(b => b.level === i);
+            if (users[0][uplineField] > 0 && bonus) {
+                await IsiBonus(title, bonus.amount, users[0][uplineField], invoice);
+                await sendNotification(users[0][uplineField], invoice, title, users[0].name,bonus.amount);
             }
         }
     } catch (error) {
-        console.error("Error in BonusPrabayar function:", error, { reff, invoice });
+        console.error(`Error in ${title} function:`, error, { reff, invoice });
         throw error;
     }
+};
+
+exports.BonusPrabayar = async (reff, invoice) => {
+    await distributeBonus(reff, invoice, "Voucher Prabayar");
+};
+
+exports.BonusPascabayar = async (reff, invoice) => {
+    await distributeBonus(reff, invoice, "Voucher Pascabayar");
 };
